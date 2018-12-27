72a438c37f3be34951c944016e3f179ba0383dc9
batenzar <batenzar@gmail.com>
2018-12-27 23:52:49 +0700

    added simple test

---
+++ src/main/java/io/github/batenzar/Util.java
@@ -0,0 +1,8 @@
+package io.github.batenzar;
+
+public class Util {
+
+	public static int countTag(String str) {
+		return 0;
+	}
+}
---
+++ src/test/java/io/github/batenzar/ReadTest.java
@@ -0,0 +1,17 @@
+package io.github.batenzar;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ReadTest {
+
+	@Test
+	public void testEmpty() {
+		Assert.assertEquals(0, Util.countTag(""));
+	}
+
+	@Test
+	public void testOne() {
+		Assert.assertEquals(1, Util.countTag("#AAA"));
+	}
+}
