b761e49609df6716f10db63c567298d4ac763c42
batenzar <batenzar@gmail.com>
2019-01-05 17:04:47 +0700

    implemented count,getList

--- src/main/java/io/github/batenzar/Util.java
+++ src/main/java/io/github/batenzar/Util.java
@@ -1,8 +1,33 @@
 package io.github.batenzar;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Util {
 
+	private static String regx = "\\#\\S+";
+	private static Pattern pattern = Pattern.compile(regx);
+
 	public static int countTag(String str) {
-		return 0;
+		Matcher matcher = pattern.matcher(str);
+		
+		int count = 0;
+        while(matcher.find()) {
+            count++;
+        }
+		return count;
+	}
+	
+	public static List<String> getTags(String str) {
+		Matcher matcher = pattern.matcher(str);
+		
+		List<String> result = new ArrayList<>();
+        while(matcher.find()) {
+        	String group = matcher.group();
+        	result.add(group);
+        }
+		return result;
 	}
 }
--- src/test/java/io/github/batenzar/ReadTest.java
+++ src/test/java/io/github/batenzar/ReadTest.java
@@ -13,11 +13,23 @@ public class ReadTest {
 	@Test
 	public void testOne() {
 		Assert.assertEquals(1, Util.countTag("#AAA"));
+		Assert.assertEquals("#AAA", Util.getTags("#AAA").get(0));
 	}
 	
 	@Test
 	public void testSpace() {
 		Assert.assertEquals(1, Util.countTag("#AA A"));
+		Assert.assertEquals("#AA", Util.getTags("#AA A").get(0));
+		
 		Assert.assertEquals(2, Util.countTag("#AA A #BB"));
+		Assert.assertEquals("#AA", Util.getTags("#AA A #BB").get(0));
+		Assert.assertEquals("#BB", Util.getTags("#AA A #BB").get(1));
 	}
+	
+	// TODO
+//	@Test
+//	public void testSharp() {
+//		Assert.assertEquals(1, Util.countTag("#AA#A"));
+//		Assert.assertEquals("#AA", Util.getTags("#AA#A").get(0));
+//	}
 }
